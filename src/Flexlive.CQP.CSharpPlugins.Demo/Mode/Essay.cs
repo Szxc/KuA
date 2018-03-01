6a4523a55f696e52d35a0523c34712713469e57f
Szxc <2575827892@qq.com>
2018-03-01 16:52:56 +0800

    修改时间格式

--- src/Flexlive.CQP.CSharpPlugins.Demo/Mode/Essay.cs
+++ src/Flexlive.CQP.CSharpPlugins.Demo/Mode/Essay.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Flexlive.CQP.CSharpPlugins.Demo.Mode
     public class Essay
     {
         public int Status { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime IntDT { get; set; }
         public string Content { get; set; }
     }
