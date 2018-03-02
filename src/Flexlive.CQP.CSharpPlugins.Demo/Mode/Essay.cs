9584e88fe5a3ec2e473e37040a967e8acffd6be0
Szxc <2575827892@qq.com>
2018-03-02 14:43:51 +0800

    时间类型

--- src/Flexlive.CQP.CSharpPlugins.Demo/Mode/Essay.cs
+++ src/Flexlive.CQP.CSharpPlugins.Demo/Mode/Essay.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace Flexlive.CQP.CSharpPlugins.Demo.Mode
     {
         public ObjectId Id { get; set; }
         public int Status { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime IntDT { get; set; }
         public string Content { get; set; }
     }
